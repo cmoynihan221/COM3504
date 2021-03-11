040f8ca6793168ae8ed637446de8588fdeb7ed18
unknown <awise1@sheffield.ac.uk>
2021-03-11 14:53:17 +0000

    implemented socket.io for sending/receiving chat messages

--- Assignment starting point/public/javascripts/index.js
+++ Assignment starting point/public/javascripts/index.js
@@ -1,6 +1,6 @@
 let name = null;
 let roomNo = null;
-let socket=null;
+let socket = io.connect();
 
 
 /**
@@ -13,7 +13,7 @@ function init() {
     document.getElementById('initial_form').style.display = 'block';
     document.getElementById('chat_interface').style.display = 'none';
 
-    //@todo here is where you should initialise the socket operations as described in teh lectures (room joining, chat message receipt etc.)
+    initSocket();
 }
 
 /**
@@ -26,13 +26,33 @@ function generateRoom() {
     document.getElementById('roomNo').value = 'R' + roomNo;
 }
 
+function initSocket() {
+    // called when someone joins the room. If it is someone else it notifies the joining of the room
+    socket.on('joined', function (room, userId) {
+        if (userId === name) {
+            // it enters the chat
+            hideLoginInterface(room, userId);
+        } else {
+            // notifies that someone has joined the room
+            writeOnHistory('<b>' + userId + '</b>' + ' joined room ' + room);
+        }
+    });
+    // called when a message is received
+    socket.on('chat', function (room, userId, chatText) {
+        let who = userId
+        if (userId === name) who = 'Me';
+        writeOnHistory('<b>' + who + ':</b> ' + chatText);
+    });
+
+}
+
 /**
  * called when the Send button is pressed. It gets the text to send from the interface
  * and sends the message via  socket
  */
 function sendChatText() {
     let chatText = document.getElementById('chat_input').value;
-    // @todo send the chat message
+    socket.emit('chat', roomNo, name, chatText);
 }
 
 /**
@@ -44,7 +64,7 @@ function connectToRoom() {
     name = document.getElementById('name').value;
     let imageUrl= document.getElementById('image_url').value;
     if (!name) name = 'Unknown-' + Math.random();
-    //@todo join the room
+    socket.emit('create or join', roomNo, name);
     initCanvas(socket, imageUrl);
     hideLoginInterface(roomNo, name);
 }
--- Assignment starting point/socket.io/socket-io.js
+++ Assignment starting point/socket.io/socket-io.js
@@ -2,7 +2,21 @@
 exports.init = function(io) {
   io.sockets.on('connection', function (socket) {
     try {
-     // insert here your event
+      /**
+       * it creates or joins a room
+       */
+      socket.on('create or join', function (room, userId) {
+        socket.join(room);
+        io.to(room).emit('joined', room, userId);
+      });
+
+      socket.on('chat', function (room, userId, chatText) {
+        io.to(room).emit('chat', room, userId, chatText);
+      });
+
+      socket.on('disconnect', function(){
+        console.log('someone disconnected');
+      });
     } catch (e) {
     }
   });
