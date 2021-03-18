cbcb4d2d946c95fae69aa8105f64dd276d9aa2a9
unknown <awise1@sheffield.ac.uk>
2021-03-18 14:03:18 +0000

    began implementing service worker

---
+++ Solution/public/service-worker.js
@@ -0,0 +1,99 @@
+// Copyright 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var dataCacheName = 'spyChatData';
+var cacheName = 'spyChat';
+var filesToCache = [
+    '/',
+    '/javascripts/app.js',
+    '/javascripts/canvas.js',
+    '/javascripts/database.js',
+    '/javascripts/index.js',
+    '/javascripts/spychatDataStructure.js',
+    '/stylesheets/style.css',
+];
+
+/**
+ * installation event: it adds all the files to be cached
+ */
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+
+/**
+ * activation of service worker: it removes all cashed files if necessary
+ */
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    /*
+     * Fixes a corner case in which the app wasn't returning the latest data.
+     */
+    return self.clients.claim();
+});
+
+
+/**
+ * this is called every time a file is fetched. This is a middleware, i.e. this method is
+ * called every time a page is fetched by the browser
+ * there are two main branches:
+ * /weather_data posts cities names to get data about the weather from the server. if offline, the fetch will fail and the
+ *      control will be sent back to Ajax with an error - you will have to recover the situation
+ *      from there (e.g. showing the cached data)
+ * all the other pages are searched for in the cache. If not found, they are returned
+ */
+self.addEventListener('fetch', function (e) {
+    console.log('[Service Worker] FetchAAAA', e.request.url);
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response
+                || fetch(e.request)
+                    .then(function (response) {
+                        // note if network error happens, fetch does not return
+                        // an error. it just returns response not ok
+                        // https://www.tjvantoll.com/2015/09/13/fetch-and-errors/
+                        if (!response.ok ||  response.statusCode>299) {
+                            console.log("error: " + response.error());
+                        } else {
+                            caches.add(response.clone());
+                            return response;
+                        }
+                    })
+                    .catch(function (err) {
+                        console.log("error: " + err);
+                    })
+        })
+    );
+});
