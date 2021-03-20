be444e7c4b8b462603323059258b8ae4f89679c1
Victoria ❤ <vickyakeke@gmail.com>
2021-03-20 22:42:55 +0300

    Adding the new page's path

--- Solution/routes/index.js
+++ Solution/routes/index.js
@@ -2,9 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Image Browsing' });
 });
+
+router.get('/', function(req, res, next) {
+  res.render('navigation', { title: 'Splash Screen' });
+});
+
+
 router.get('/media', function(req, res, next) {
   res.render('mediaCapture', { title: 'Media Capture' });
 });
