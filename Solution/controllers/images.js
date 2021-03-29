bec13e113845b266f571d2e1fb6147ca1f6f60d2
Victoria ❤ <vickyakeke@gmail.com>
2021-03-29 18:01:17 +0300

    Adding method that allows for rendering all added images

--- Solution/controllers/images.js
+++ Solution/controllers/images.js
@@ -2,12 +2,12 @@ let Image = require('../models/images');
 const fs = require('fs');
 exports.saveImage = function(req, res, next){
     //drop the images db
-    Image.remove({}, function(err) {
+    /*Image.remove({}, function(err) {
           console.log('collection removed')
-    });
+    });*/
         let userId = req.body.userId;
         let currentTime = new Date().getTime();
-        let directory = './images/' + userId + '/';
+        let directory = './public/images/' + userId + '/';
         if(!fs.existsSync(directory)){
             try {
                 fs.mkdirSync(directory,  { recursive: true });
@@ -28,11 +28,42 @@ exports.saveImage = function(req, res, next){
         console.log('file saved!');
         let image_data = new Image({
             user: userId,
-            filePath: filePath
+            file_path: path
         });
         image_data.save(function(err, results){
             console.log(err)
             console.log('Results:', results);
         })
+    Image.find(
+        function (err, images) {
+            if (err)
+                res.status(500).send('Invalid data!');
+            //let character =null;
+            console.log(images);
+            //res.setHeader('Content-Type', 'application/json');
+            //res.send(JSON.stringify(images));
+            //res.render('images', { title: JSON.stringify(images) });
+        });
     res.end(JSON.stringify(image_data));
+};
+
+//fn that renders images on the page
+exports.renderImages = function(req, res, next){
+    //find all images and send in the list of the filepaths as json to the receiving page
+    //render the images on the receiving page
+    try {
+        Image.find(
+            function (err, images) {
+                if (err) {
+                    res.status(500).send('Invalid data!');
+                }
+                //let character =null;
+                console.log(images);
+                //res.setHeader('Content-Type', 'application/json');
+                //res.send(JSON.stringify(images));
+
+            }).then(results =>res.render('images', { images: results }));
+    } catch (e) {
+        res.status(500).send('error '+ e);
+    }
 };
